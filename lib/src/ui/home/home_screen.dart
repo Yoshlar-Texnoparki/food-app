b5668cbf8479723ced94cb0863822170aac51039
Mukhammadjon <sheykxmuhammad@gmail.com>
2025-06-14 17:21:44 +0500

    home screen UI tugallandi bunda biz ListView.builder va unging proportieslarini organdik

--- lib/src/ui/home/home_screen.dart
+++ lib/src/ui/home/home_screen.dart
@@ -8,15 +8,50 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 2;
+  List<Category> categories = [
+    Category(name: "All", image: Icon(Icons.fastfood)),
+    Category(name: "Burger", image: Icon(Icons.fastfood)),
+    Category(name: "Pizza", image: Icon(Icons.fastfood)),
+    Category(name: "Hot-Dog", image: Icon(Icons.fastfood)),
+    Category(name: "KFC", image: Icon(Icons.fastfood)),
+    Category(name: "Drink", image: Icon(Icons.fastfood)),
+    Category(name: "Tea", image: Icon(Icons.fastfood)),
+  ];
+  List<Restaurant> restaurant = [
+    Restaurant(
+        name: "Rose garden restaurant",
+        delivered: "free",
+        foods: "Burger - Chiken - Riche - Wings ",
+        rating: 4.7,
+        time: "20 min"),
+    Restaurant(
+        name: "Musaffo",
+        delivered: "free",
+        foods: "Somsa - Lagmon - Osh - Shashlik ",
+        rating: 4.7,
+        time: "20 min"),
+    Restaurant(
+        name: "Banonza",
+        delivered: "free",
+        foods: "Tort - Shirinliklar - Coffe - Choy ",
+        rating: 4.7,
+        time: "10 min"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(onPressed: (){}, icon: Icon(Icons.menu)),
+        centerTitle: false,
+        leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
         title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("Deliver to"),
-            Text("Halal lab office"),
+            Text("Deliver to",
+                style: TextStyle(fontSize: 18, color: Colors.deepOrangeAccent)),
+            Text("Halal lab office",
+                style: TextStyle(fontSize: 18, color: Colors.black)),
           ],
         ),
         actions: [
@@ -25,14 +60,16 @@ class _HomeScreenState extends State<HomeScreen> {
             height: 45,
             margin: EdgeInsets.only(right: 8),
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(50),
-              color: Colors.black
-            ),
+                borderRadius: BorderRadius.circular(50), color: Colors.black),
             child: Stack(
               children: [
                 IconButton(
-                    onPressed: (){},
-                    icon: Icon(Icons.shopping_bag,size: 30,color: Colors.white,)),
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.shopping_bag,
+                      size: 30,
+                      color: Colors.white,
+                    )),
                 Positioned(
                   right: 0,
                   top: 0,
@@ -41,10 +78,12 @@ class _HomeScreenState extends State<HomeScreen> {
                     width: 20,
                     height: 20,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      color: Colors.orange
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.orange),
+                    child: Text(
+                      "2",
+                      style: TextStyle(color: Colors.white),
                     ),
-                    child: Text("2",style: TextStyle(color: Colors.white),),
                   ),
                 )
               ],
@@ -52,6 +91,204 @@ class _HomeScreenState extends State<HomeScreen> {
           )
         ],
       ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Text(
+                    "Hey Halal,",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Text(
+                    " Good Afternoon!",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                ],
+              ),
+              Container(
+                alignment: Alignment.center,
+                height: 62,
+                margin: EdgeInsets.only(top: 16, bottom: 32),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Color(0xFFF6F6F6),
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.search),
+                      border: InputBorder.none,
+                      hintText: "Search dishes, restaurants"),
+                ),
+              ),
+              Row(
+                children: [
+                  Text(
+                    "All Categories",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Spacer(),
+                  TextButton(onPressed: () {}, child: Text("See All")),
+                  Icon(Icons.arrow_forward_ios_rounded),
+                ],
+              ),
+              SizedBox(
+                height: 60,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: categories.length,
+                    itemBuilder: (ctx, index) {
+                      return GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            selectedIndex = index;
+                          });
+                        },
+                        child: Container(
+                          margin: EdgeInsets.symmetric(horizontal: 8),
+                          padding: EdgeInsets.symmetric(horizontal: 12),
+                          alignment: Alignment.center,
+                          height: 60,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              color: selectedIndex == index
+                                  ? Color(0xFFFFD27C)
+                                  : Colors.grey),
+                          child: Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(right: 8),
+                                height: 44,
+                                width: 44,
+                                decoration: BoxDecoration(
+                                  color: Colors.grey,
+                                  borderRadius: BorderRadius.circular(50),
+                                ),
+                                child: categories[index].image,
+                              ),
+                              Text(categories[index].name),
+                              SizedBox(
+                                width: 8,
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    }),
+              ),
+              SizedBox(
+                height: 32,
+              ),
+              Row(
+                children: [
+                  Text(
+                    "Open Restaurants",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Spacer(),
+                  TextButton(onPressed: () {}, child: Text("See All")),
+                  Icon(Icons.arrow_forward_ios_rounded),
+                ],
+              ),
+            ListView.builder(
+              itemCount: restaurant.length,
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+                itemBuilder: (ctx,index){
+              return SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 225,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      height: 137,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.grey),
+                    ),
+                    Text(
+                      restaurant[index].name,
+                      style: TextStyle(fontSize: 23),
+                    ),
+                    Text(
+                      restaurant[index].foods,
+                      style: TextStyle(fontSize: 14, color: Colors.grey),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.star_border,
+                          color: Colors.orange,
+                        ),
+                        Text(
+                          restaurant[index].rating.toString(),
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          width: 22,
+                        ),
+                        Icon(
+                          Icons.delivery_dining_outlined,
+                          color: Colors.orange,
+                        ),
+                        Text(
+                          restaurant[index].delivered,
+                          style: TextStyle(fontWeight: FontWeight.normal),
+                        ),
+                        SizedBox(
+                          width: 22,
+                        ),
+                        Icon(
+                          Icons.access_time_sharp,
+                          color: Colors.orange,
+                        ),
+                        Text(
+                          restaurant[index].time,
+                          style: TextStyle(fontWeight: FontWeight.normal),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            })
+            ],
+          ),
+        ),
+      ),
     );
   }
+}
+
+class Category {
+  String name;
+  Icon image;
+
+  Category({required this.name, required this.image});
+}
+
+class Restaurant {
+  String name;
+  String foods;
+  double rating;
+  String delivered;
+  String time;
+
+  Restaurant(
+      {required this.name,
+      required this.delivered,
+      required this.foods,
+      required this.rating,
+      required this.time});
 }
